9c681130887ed4dfdfc5f1ad1c7750c6c46a1cf3
garin <garin54@gmail.com>
2013-12-01 22:08:03 +0900

    プロジェクトを raf から raf-parser に変更

--- lib/raf-parser.rb
+++ lib/raf-parser.rb
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) garin <garin54@gmail.com> 2011-2013
 # See the included file COPYING for details.
+require "rafblockparser.tab"
+
 module Raf
   VERSION = File.readlines(File.join(File.dirname(__FILE__),"../VERSION"))[0].strip
   RELEASE = File.readlines(File.join(File.dirname(__FILE__),"../RELEASE"))[0].strip
